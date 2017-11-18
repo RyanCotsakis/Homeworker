fadc126addb2d8154d4a149d1e3414940b13e36a
Ryan Cotsakis <ryancotsakis@gmail.com>
2017-11-17 20:52:49 -0800

    v0.5 added day length thing

--- homeworker.py
+++ homeworker.py
@@ -1,6 +1,7 @@
 """ homeworker - Ryan Cotsakis """
 
-import win32gui, win32con
+import win32gui
+import win32con
 import os
 import sys
 import time
@@ -8,240 +9,298 @@ import threading
 from datetime import date
 
 paused = False
-commands = []
-currentTasks = []
-completedTasks = []
+listener_died = False
+tracking_today = True
 
-DELAY = 0.1 #seconds per cycle (2*DELAY < WAIT)
-WAIT = 0.2 #seconds for >>
-CURRENTHEADER = "CURRENT TASKS:"
-COMPLETEDHEADER = "COMPLETED TASKS:"
+commands = []
+current_tasks = []
+completed_tasks = []
+days = []
+
+DELAY = 0.1  # seconds per cycle (2*DELAY < WAIT)
+WAIT = 0.2  # seconds for >>
+
+CURRENT_HEADER = "CURRENT TASKS:"
+COMPLETED_HEADER = "COMPLETED TASKS:"
+DAYS_HEADER = "TIME PER DAY:"
+DAYS_SEPARATOR = " - "
+DATE_FORMAT = '%d.%m.%Y'
+
 CLEAR_COMMAND = "cls"
 OPEN_COMMAND = "start notepad log.txt"
 
-def listener():
-	time.sleep(WAIT)
-	while True:
-		if not paused:
-			command = raw_input("\n>> ").strip()
-		else:
-			command = raw_input("\nPAUSED >> ").strip()
-		commands.append(command)
-		if len(command) and ((command in "exit" and command[0] == "e") or (command in "open" and command[0] == "o")):
-			return
-		time.sleep(WAIT)
 
-def update(file):
-	data = [CURRENTHEADER]+currentTasks+[COMPLETEDHEADER]+completedTasks
-	file.seek(0)
-	file.write('\n'.join(data))
-	file.truncate()
-	return data
+def listener():
+    global listener_died
+    time.sleep(WAIT)
+
+    while True:
+        if not paused and not tracking_today:
+            command = raw_input("\n>> ").strip()
+        elif paused and not tracking_today:
+            command = raw_input("\n(P) >> ").strip()
+        elif not paused and tracking_today:
+            command = raw_input("\n(T) >> ").strip()
+        elif paused and tracking_today:
+            command = raw_input("\n(P,T) >> ").strip()
+
+        commands.append(command)
+
+        if len(command) and ((command in "exit" and command[0] == "e") or (command in "open" and command[0] == "o")):
+            listener_died = True
+            return
+
+        time.sleep(WAIT)
+
+
+def update(_file):
+    data = [CURRENT_HEADER] + current_tasks + [COMPLETED_HEADER] + completed_tasks + [DAYS_HEADER] + days
+    _file.seek(0)
+    _file.write('\n'.join(data))
+    _file.truncate()
+    return data
+
+
+def add_minute(time_string):
+    try:
+        time_string = time_string.split(':')
+        time_string_m = int(time_string[1])
+        time_string_h = int(time_string[0])
+        if time_string_m < 59:
+            time_string_m += 1
+        else:
+            time_string_m = 0
+            time_string_h += 1
+        return str(time_string_h) + ':' + str(time_string_m).zfill(2)
+    except (ValueError, IndexError):
+        return None
+
 
 def main():
-	global paused
-	try:
-		f = open("./log.txt",mode = "r+")
-		data = f.read().splitlines()
-	except IOError:
-		f = open("./log.txt",mode = "w")
-		data = update(f)
+    global paused
+    global tracking_today
 
-	inCurrent = True
-	for item in data:
-		if (item != COMPLETEDHEADER and item != CURRENTHEADER and inCurrent):
-			currentTasks.append(item)
-		elif item == COMPLETEDHEADER:
-			inCurrent = False
-		elif not inCurrent:
-			completedTasks.append(item)
+    try:
+        f = open("./log.txt", mode="r+")
+        data = f.read().splitlines()
+    except IOError:
+        f = open("./log.txt", mode="w")
+        data = update(f)
 
+    stage = 0
+    for item in data:
+        if item == CURRENT_HEADER and stage == 0:
+            stage += 1
+        elif stage == 1:  # current tasks
+            if item == COMPLETED_HEADER:
+                stage += 1
+            else:
+                current_tasks.append(item)
+        elif stage == 2:  # completed tasks
+            if item == DAYS_HEADER:
+                stage += 1
+            else:
+                completed_tasks.append(item)
+        elif stage == 3:  # days
+            days.append(item)
 
-	commands.append('info')
-	commands.append('current')
+    commands.append('info')
+    commands.append('current')
 
-	p = threading.Thread(target = listener)
-	p.start()
-	cycleCount = 0
-	while True:
-		if not paused:
-			cycleCount += 1
+    p = threading.Thread(target=listener)
+    p.start()
 
-		if not cycleCount % (60/DELAY):
-			if len(currentTasks):
-				try:
-					currentTask = currentTasks[0].split('\t')
-					terms = len(currentTask)
-					currentTaskHM = currentTask[terms-1].split(':')
-					currentTaskM = int(currentTaskHM[1])
-					currentTaskH = int(currentTaskHM[0])
-					if currentTaskM < 59:
-						currentTaskM += 1
-					else:
-						currentTaskM = 0
-						currentTaskH += 1
-					currentTasks[0] = '\t'.join(currentTask[:terms-1]) + '\t' + str(currentTaskH) + ':' + str(currentTaskM).zfill(2)
-				except (ValueError, IndexError):
-					pass
-			data = update(f)
+    cycle_count = 0
+    while True:
+        if not paused:
+            cycle_count += 1
 
+        if listener_died:
+            break
 
-		if len(commands):
-			command = commands[0]
-			del commands[0]
+        if not cycle_count % (60 / DELAY):
+            if len(current_tasks):
+                    current_task = current_tasks[0].split('\t')
+                    current_tasks[0] = '\t'.join(current_task[:-1]) + '\t' + add_minute(current_task[-1])
 
-			if not len(command):
-				pass
+            if tracking_today:
+                today, today_index = None, None
+                for i, day in enumerate(days):
+                    day = day.split(DAYS_SEPARATOR)
+                    if date.today().strftime(DATE_FORMAT) == day[0]:
+                        today = day
+                        today_index = i
+                        break
+                if today is not None:
+                    today[1] = add_minute(today[1])
+                    days[today_index] = DAYS_SEPARATOR.join(today)
+                else:
+                    days.append(date.today().strftime(DATE_FORMAT + DAYS_SEPARATOR) + "0:01")
 
-			elif command in "log" and command[0] == "l":
-				print '\n'.join(data)
+            data = update(f)
 
-			elif command in "exit" and command[0] == "e":
-				data = update(f)
-				break
+        if len(commands):
+            command = commands[0]
+            del commands[0]
 
-			elif command in "minimize" and command[0] == "m" and not command[:3] == "miz":
-				Minimize = win32gui.GetForegroundWindow()
-				win32gui.ShowWindow(Minimize, win32con.SW_MINIMIZE)
+            if not len(command):
+                pass
 
-			elif command in "unpaused" and (command[0] == "p" or command[0] == "u"):
-				paused = not paused
+            elif command in "log" and command[0] == "l":
+                print '\n'.join(data)
 
-			elif command in "info" and command[0] == "i":
-				print "\nCOMMAND LIST:"
-				print "add\t\t-add a task"
-				print "clear\t\t-clear the output screen"
-				print "continue\t-continue a completed task"
-				print "current\t\t-display unfinished tasks"
-				print "done\t\t-log the current task as complete"
-				print "delete\t\t-delete an unfinished task"
-				print "exit\t\t-close program"
-				print "info\t\t-repeat these instructions"
-				print "log\t\t-view the log of completed tasks"
-				print "minimize\t-minimize homeworker"
-				print "open\t\t-close program, and open log.txt"
-				print "(un)pause\t-stop/start adding time to current task"
-				print "switch\t\t-switch to a different task"
-				print "\nThe time after each task corresponds to the amount\nof time it has been actively worked on.\n"
+            elif command in "minimize" and command[0] == "m" and not command[:3] == "miz":
+                minimize = win32gui.GetForegroundWindow()
+                win32gui.ShowWindow(minimize, win32con.SW_MINIMIZE)
 
-			elif command in "add" and command[0] == "a":
-				print "Enter New Line:"
-				sys.stdout.flush()
-				while not len(commands):
-					time.sleep(DELAY)
-					sys.stdout.flush()
-				if len(commands[0]):
-					newLine = commands[0] + "\t0:00"
-					del commands[0]
-					currentTasks.append(newLine)
-					data = update(f)
-					print "Task added!"
-				else:
-					print "Task name must not be empty."
+            elif (command in "unpaused" and (command[0] == "p" or command[0] == "u")) or command == "P":
+                paused = not paused
 
-			elif command in "open" and command[0] == "o":
-				try:
-					f.close()
-					time.sleep(DELAY)
-					os.system(OPEN_COMMAND)
-					return
-				except:
-					print "Could not open log.txt"
+            elif command in "info" and command[0] == "i":
+                print "\nCOMMAND LIST:"
+                print "add(i)\t\t-add a task at index i"
+                print "clear\t\t-clear the output screen"
+                print "continue\t-continue a completed task"
+                print "current\t\t-display unfinished tasks"
+                print "done\t\t-log the current task as complete"
+                print "delete\t\t-delete an unfinished task"
+                print "exit\t\t-close program"
+                print "info\t\t-repeat these instructions"
+                print "log\t\t-view the log of completed tasks"
+                print "minimize\t-minimize homeworker"
+                print "open\t\t-close program, and open log.txt"
+                print "(un)pause\t-stop/start adding time to current task (P)"
+                print "today\t\t-toggle the option to track the day (T)"
+                print "switch\t\t-switch to a different task"
+                print "\nThe time after each task corresponds to the amount\nof time it has been actively worked on.\n"
 
+            elif (command in "today" and command[0] == "t") or command == "T":
+                tracking_today = not tracking_today
 
-			elif (command in "current" and command[:2] == "cu") or command == "ls":
-				if len(currentTasks):
-					print "Your current tasks are:"
-					print '\n'.join(currentTasks)
-				else:
-					print "You're all done!"
+            elif (command in "add" and command[0] == "a") or command[:3] == "add":
+                index = None
+                if len(command) > 3:
+                    try:
+                        index = int(command[3:])
+                        command = "..."  # Makes len(command) == 3
+                    except ValueError:
+                        print "Invalid command, '" + command + "'."
+                if len(command) <= 3:
+                    print "Enter New Line:"
+                    sys.stdout.flush()
+                    while not len(commands):
+                        time.sleep(DELAY)
+                        sys.stdout.flush()
+                    if len(commands[0]) and not listener_died:
+                        new_line = commands[0] + "\t0:00"
+                        del commands[0]
+                        if index is not None:
+                            current_tasks.insert(index-1, new_line)
+                        else:
+                            current_tasks.append(new_line)
+                        data = update(f)
+                        print "Task added!"
+                    elif not listener_died:
+                        print "Task name must not be empty."
 
-			elif command in "done" and command[:2] == "do":
-				try:
-					finishedTask = currentTasks[0]
-					del currentTasks[0]
-					completedTasks.append(date.today().strftime('%d.%m.%Y - ') + finishedTask)
-					data = update(f)
-					print "Well done!"
-				except IndexError:
-					print "Done what?"
+            elif command in "open" and command[0] == "o":
+                f.close()
+                time.sleep(DELAY)
+                os.system(OPEN_COMMAND)
+                return
 
-			elif command in "delete" and command[:2] == "de":
-				itemNum = 1
-				for item in currentTasks:
-					print str(itemNum) + ": " + item
-					itemNum += 1
-				print "\nType the number corresponding to the task you'd like\nto delete:"
-				sys.stdout.flush()
-				while not len(commands):
-					time.sleep(DELAY)
-					sys.stdout.flush()
-				try:
-					itemNum = int(commands[0])-1
-					del currentTasks[itemNum]
-					data = update(f)
-					print "Task Deleted."
-					del commands[0]
-				except (ValueError, IndexError):
-					print "Could not delete task '" + commands[0] + "'"
+            elif (command in "current" and command[:2] == "cu") or command == "ls":
+                if tracking_today:
+                    print days[-1]
+                if len(current_tasks):
+                    print "Your current tasks are:"
+                    print '\n'.join(current_tasks)
+                else:
+                    print "You're all done!"
 
+            elif command in "done" and command[:2] == "do":
+                try:
+                    finished_task = current_tasks[0]
+                    del current_tasks[0]
+                    completed_tasks.append(date.today().strftime(DATE_FORMAT + DAYS_SEPARATOR) + finished_task)
+                    data = update(f)
+                    print "Well done!"
+                except IndexError:
+                    print "Done what?"
 
-			elif command in "clear" and command[:2] == "cl":
-				os.system(CLEAR_COMMAND)
+            elif command in "delete" and command[:2] == "de":
+                item_num = 1
+                for item in current_tasks:
+                    print str(item_num) + ": " + item
+                    item_num += 1
+                print "\nType the number corresponding to the task you'd like\nto delete:"
+                sys.stdout.flush()
+                while not len(commands):
+                    time.sleep(DELAY)
+                    sys.stdout.flush()
+                try:
+                    item_num = int(commands[0]) - 1
+                    del current_tasks[item_num]
+                    data = update(f)
+                    print "Task Deleted."
+                    del commands[0]
+                except (ValueError, IndexError):
+                    print "Could not delete task '" + commands[0] + "'"
 
-			elif (command in "switch" and command[0] == "s") or command == "cd":
-				itemNum = 1
-				for item in currentTasks:
-					print str(itemNum) + ": " + item
-					itemNum += 1
-				print "\nType the number corresponding to the task you'd like\nto switch to:"
-				sys.stdout.flush()
-				while not len(commands):
-					time.sleep(DELAY)
-					sys.stdout.flush()
-				try:
-					itemNum = int(commands[0])-1
-					currentTask = currentTasks[itemNum]
-					del currentTasks[itemNum]
-					currentTasks.insert(0,currentTask)
-					data = update(f)
-					print "Successfully switched to task " + str(itemNum+1) + "."
-					del commands[0]
-				except (ValueError, IndexError):
-					print "Could not switch to task '" + commands[0] + "'"
+            elif command in "clear" and command[:2] == "cl":
+                os.system(CLEAR_COMMAND)
 
-			elif command in "continue" and command[:2] == "co":
-				itemNum = 1
-				for item in completedTasks:
-					print str(itemNum) + ": " + item
-					itemNum += 1
-				print "\nType the number corresponding to the task you'd like\nto continue:"
-				sys.stdout.flush()
-				while not len(commands):
-					time.sleep(DELAY)
-					sys.stdout.flush()
-				try:
-					itemNum = int(commands[0])-1
-					currentTask = completedTasks[itemNum]
-					del completedTasks[itemNum]
-					currentTasks.insert(0,currentTask)
-					data = update(f)
-					print "Successfully switched to task " + str(itemNum+1) + "."
-					del commands[0]
-				except (ValueError, IndexError):
-					print "Could not continue task '" + commands[0] + "'."
+            elif (command in "switch" and command[0] == "s") or command == "cd":
+                item_num = 1
+                for item in current_tasks:
+                    print str(item_num) + ": " + item
+                    item_num += 1
+                print "\nType the number corresponding to the task you'd like\nto switch to:"
+                sys.stdout.flush()
+                while not len(commands):
+                    time.sleep(DELAY)
+                    sys.stdout.flush()
+                try:
+                    item_num = int(commands[0]) - 1
+                    current_task = current_tasks[item_num]
+                    del current_tasks[item_num]
+                    current_tasks.insert(0, current_task)
+                    data = update(f)
+                    print "Successfully switched to task " + str(item_num + 1) + "."
+                    del commands[0]
+                except (ValueError, IndexError):
+                    print "Could not switch to task '" + commands[0] + "'"
 
-			else:
-				print "Invalid command, '" + command + "'."
+            elif command in "continue" and command[:2] == "co":
+                item_num = 1
+                for item in completed_tasks:
+                    print str(item_num) + ": " + item
+                    item_num += 1
+                print "\nType the number corresponding to the task you'd like\nto continue:"
+                sys.stdout.flush()
+                while not len(commands):
+                    time.sleep(DELAY)
+                    sys.stdout.flush()
+                try:
+                    item_num = int(commands[0]) - 1
+                    current_task = completed_tasks[item_num]
+                    del completed_tasks[item_num]
+                    current_tasks.insert(0, current_task)
+                    data = update(f)
+                    print "Successfully switched to task " + str(item_num + 1) + "."
+                    del commands[0]
+                except (ValueError, IndexError):
+                    print "Could not continue task '" + commands[0] + "'."
 
-		sys.stdout.flush()
-		time.sleep(DELAY)
-		# End of while(True)
+            else:
+                print "Invalid command, '" + command + "'."
 
-	try:
-		f.close()
-	except:
-		pass
+        sys.stdout.flush()
+        time.sleep(DELAY)
+    # End of while(True)
+
+    f.close()
+
 
 if __name__ == '__main__':
-	main()
+    main()
